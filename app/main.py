a16e8b1c339fcdde9b6051a4fb5400c5d5bd4399
student <student@local.ru>
2019-02-24 09:04:28 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,27 @@
+# stars = "*****"
+# counter = 0
+#
+# while counter < 5:
+#     counter += 1
+#     print(stars)
+#
+# i = 0
+# while i < 3:
+#     j = 0
+#     while j < 3:
+#         print(i, j)
+#         j += 1
+#     i += 1
+
+line = '*'
+i = 0
+while i < 5:
+    j = 0
+    while j <= i:
+        print(line, end='')
+        j += 1
+    print('')
+    i += 1
+
+
+
